2fa9a23a49aadb09ff128994d10a5e6a7d95c0cf
Sergey Lyubimov <lysevi@gmail.com>
2015-01-18 08:36:43 +0300

    CMake

--- test/common_meas_test.cpp
+++ test/common_meas_test.cpp
@@ -8,9 +8,9 @@
 BOOST_AUTO_TEST_CASE(MeasEmpty) {
 	//common::Meas::PMeas pm = common::Meas::empty();
 	common::Meas m;
-	BOOST_CHECK_EQUAL(m.data, 0);
-	BOOST_CHECK_EQUAL(m.flag, 0);
-	BOOST_CHECK_EQUAL(m.id,   0);
-	BOOST_CHECK_EQUAL(m.source, 0);
-	BOOST_CHECK_EQUAL(m.time, 0);
+	BOOST_CHECK_EQUAL(m.data, common::Value(0));
+	BOOST_CHECK_EQUAL(m.flag, common::Flag(0));
+	BOOST_CHECK_EQUAL(m.id,   common::Id(0));
+	BOOST_CHECK_EQUAL(m.source, common::Flag(0));
+	BOOST_CHECK_EQUAL(m.time, common::Time(0));
 }

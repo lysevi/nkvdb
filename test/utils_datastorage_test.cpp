040f24865e011781bc0703f1c34ca7a071a2ab7b
Sergey Lyubimov <lysevi@gmail.com>
2015-01-18 16:44:25 +0300

    simple unit test for datastorage. not implemented yet.

--- test/utils_datastorage_test.cpp
+++ test/utils_datastorage_test.cpp
@@ -7,3 +7,39 @@ BOOST_AUTO_TEST_CASE(ctor) {
     utils::DataStorage*ds=mdb_test::makeDS();
     BOOST_CHECK_EQUAL(true, true);
 }
+
+BOOST_AUTO_TEST_CASE(writeValues) {
+    utils::DataStorage*ds=mdb_test::makeDS();
+    auto ids=mdb_test::getIds();
+    auto tm=mdb_test::testMeases(ids);
+    BOOST_CHECK_EQUAL(true, false);
+}
+
+
+BOOST_AUTO_TEST_CASE(readValues) {
+    utils::DataStorage*ds=mdb_test::makeDS();
+    auto ids=mdb_test::getIds();
+    auto tm=mdb_test::testMeases(ids);
+    BOOST_CHECK_EQUAL(true, false);
+}
+
+BOOST_AUTO_TEST_CASE(readValuesInterval) {
+    utils::DataStorage*ds=mdb_test::makeDS();
+    auto ids=mdb_test::getIds();
+    auto tm=mdb_test::testMeases(ids);
+    BOOST_CHECK_EQUAL(true, false);
+}
+
+BOOST_AUTO_TEST_CASE(readValuesFltr) {
+    utils::DataStorage*ds=mdb_test::makeDS();
+    auto ids=mdb_test::getIds();
+    auto tm=mdb_test::testMeases(ids);
+    BOOST_CHECK_EQUAL(true, false);
+}
+
+BOOST_AUTO_TEST_CASE(readValuesByDate) {
+    utils::DataStorage*ds=mdb_test::makeDS();
+    auto ids=mdb_test::getIds();
+    auto tm=mdb_test::testMeases(ids);
+    BOOST_CHECK_EQUAL(true, false);
+}

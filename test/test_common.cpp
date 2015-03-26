355d69c4bf22329d96fb72209c8fe592632978bf
lsv <lsv@gmail.com>
2015-03-26 15:56:28 +0300

    empty!

--- test/test_common.cpp
+++ test/test_common.cpp
@@ -2,33 +2,6 @@
 
 namespace mdb_test {
 
-    common::IdArray getIds() {
-        common::IdArray result{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        return result;
-    }
-
-    utils::ICache::PCache makeDS() {
-        auto ids = mdb_test::getIds();
-        utils::ICache::PCache lcache(new utils::LinearCache(ids));
-        utils::ICache::PCache excache(new utils::MemoryCache(testCacheSz, ids));
-        utils::ICache::PCache result(new utils::DataStorage(lcache, excache));
-        return result;
-    }
-
-    common::Meas::MeasArray testMeases(const common::IdArray &ids) {
-        common::Meas::MeasArray ma;
-        ma.resize(ids.size());
-        int i = 0;
-        for (auto id:ids) {
-            auto m = std::make_shared<common::Meas>();
-            m->id = id;
-            m->time = id * 10;
-            m->data = id;
-            m->flag = id;
-            ma[i] = m;
-            i++;
-        }
-        return ma;
-    }
+    
 
 }
--- test/test_common.h
+++ test/test_common.h
@@ -1,16 +1,8 @@
-
 #pragma once
 #include <common/common.h>
 #include <common/Meas.h>
-#include <utils/LinearCache.h>
-#include <utils/DataStorage.h>
-#include <utils/MemoryCache.h>
+
 
 namespace mdb_test {
-	const size_t testCacheSz = 1 * 1024 * 1024; //1 mb
-
-    common::IdArray getIds();
-	utils::ICache::PCache makeDS();
-
-    common::Meas::MeasArray testMeases(const common::IdArray &ids);
+	
 }

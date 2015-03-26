f8895ee38bf1e1d2ec893bbadc6d6e9557373dc8
lsv <lsv@gmail.com>
2015-03-26 16:11:08 +0300

    remove tests

--- test/empty.cpp
+++ test/empty.cpp
@@ -1,8 +1,6 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
 #include <boost/test/unit_test.hpp>
-#include <utils/utils.h>
-
 
 BOOST_AUTO_TEST_CASE(testSimple) {
     BOOST_CHECK_EQUAL(true, true);

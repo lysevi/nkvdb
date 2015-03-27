c0d382ffdb6a33fe9c6462e1353fae1a04e80ca0
lsv <lsv@gmail.com>
2015-03-27 08:09:31 +0300

    utils: using boost:logger.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
+#include "utils/ProcessLogger.h"
 
 int main(int argc,char**argv){
-    std::cout<<"Hello world!"<<std::endl;
+	logger << "Start!";
 }
--- utils/ProcessLogger.h
+++ utils/ProcessLogger.h
@@ -1,29 +1,8 @@
 #pragma once
 #include <string>
 #include <cstdint>
-#include "ILogWriter.h"
+#include <boost/log/trivial.hpp>
 
-#define logger common::ProcessLogger::get()->GetStream()
 using std::endl;
 
-namespace common
-{
-	class ProcessLogger : public ILogWriter
-	{
-	public:
-		static ProcessLogger* get();
-		static void start();
-		static void stop();
-		virtual std::ostream& GetStream();
-		void setLevel(uint16_t level) {
-			m_level = level;
-		}
-		virtual ~ProcessLogger() {
-		};
-	private:
-		uint16_t m_level;
-		static ProcessLogger *m_instance;
-		ProcessLogger() {
-		};
-	};
-}
+#define logger       BOOST_LOG_TRIVIAL(info)

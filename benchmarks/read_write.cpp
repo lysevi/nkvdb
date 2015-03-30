377642f1e418a26f52674f0e9ab08c4b4ecf1e17
lsv <lsv@gmail.com>
2015-03-30 11:54:25 +0300

    benchmark: read_write now parse cmd args.

--- benchmarks/read_write.cpp
+++ benchmarks/read_write.cpp
@@ -1,10 +1,42 @@
 #include <ctime>
+#include <iostream>
 #include <storage/storage.h>
 #include <utils/ProcessLogger.h>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int meas2write = 10;
+int write_iteration = 50;
+bool write_only = false;
 
 int main(int argc, char*argv[]) {
-	const int meas2write = 10;
-	const int write_iteration = 50;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce help message")
+		("mc", po::value<int>(), "measurment count")
+		("ic", po::value<int>(), "iteration count")
+		("write-only",  "don`t run readInterval")
+		;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << desc << std::endl;
+		return 1;
+	}
+	
+	if (vm.count("write-only")) {
+		write_only = true;
+	}
+
+	if (vm.count("mc"))
+		meas2write = vm["mc"].as<int>();
+	if (vm.count("ic"))
+		write_iteration = vm["ic"].as<int>();
+	
 	const uint64_t storage_size = sizeof(storage::Page::Header) + (sizeof(storage::Meas)*meas2write);
 	const std::string storage_path = "benchmarkStorage";
 	logger << "storage page size: " << storage_size << endl
@@ -29,7 +61,7 @@ int main(int argc, char*argv[]) {
 		ds = nullptr;
 		auto pages = utils::ls(storage_path);
 	}
-	{
+	if (!write_only){
 		storage::DataStorage::PDataStorage ds = storage::DataStorage::Open(storage_path);
 		clock_t read_t0 = clock();
 		for (int i = 1; i < meas2write*write_iteration; ++i) {

c56c7598d6c0a94bd1e34dd22970d8d6755013d5
lsv <lsv@gmail.com>
2015-03-30 11:42:12 +0300

    test: create storate_page_test.

--- test/storage_test.cpp
+++ test/storage_test.cpp
@@ -24,67 +24,6 @@ BOOST_AUTO_TEST_CASE(MeasEmpty) {
     delete pm;
 }
 
-BOOST_AUTO_TEST_CASE(PageCreateOpen) {
-    {
-        Page::PPage created = Page::Create(mdb_test::test_page_name, mdb_test::sizeInMb10);
-        BOOST_CHECK(!created->isFull());
-    }
-    {
-        Page::PPage openned = Page::Open(mdb_test::test_page_name);
-
-        BOOST_CHECK_EQUAL(openned->size(), mdb_test::sizeInMb10);
-        BOOST_CHECK(!openned->isFull());
-    }
-}
-
-BOOST_AUTO_TEST_CASE(PageIO) {
-    const int TestableMeasCount = 10000;
-    {
-        {
-            Page::PPage storage = Page::Create(mdb_test::test_page_name, mdb_test::sizeInMb10);
-        }
-        const int flagValue = 1;
-        const int srcValue = 2;
-        const int timeValue = 3;
-
-        for (int i = 0; i < TestableMeasCount; ++i) {
-            Page::PPage storage = Page::Open(mdb_test::test_page_name);
-            auto newMeas = storage::Meas::empty();
-            newMeas->value = i;
-            newMeas->id = i;
-            newMeas->flag = flagValue;
-            newMeas->source = srcValue;
-            newMeas->time = i;
-            storage->append(newMeas);
-            delete newMeas;
-        }
-
-        Page::PPage storage = Page::Open(mdb_test::test_page_name);
-
-        BOOST_CHECK_EQUAL(storage->minTime(), 0);
-        BOOST_CHECK_EQUAL(storage->maxTime(), TestableMeasCount-1);
-
-        auto newMeas = storage::Meas::empty();
-        for (int i = 0; i < TestableMeasCount; ++i) {
-            bool readState = storage->read(newMeas, i);
-
-            BOOST_CHECK_EQUAL(readState, true);
-            BOOST_CHECK_EQUAL(newMeas->value, i);
-            BOOST_CHECK_EQUAL(newMeas->id, i);
-            BOOST_CHECK_EQUAL(newMeas->flag, flagValue);
-            BOOST_CHECK_EQUAL(newMeas->source, srcValue);
-
-            BOOST_CHECK_EQUAL(newMeas->time, i);
-        }
-        BOOST_CHECK(!storage->isFull());
-        delete newMeas;
-
-        auto hdr=storage->getHeader();
-        BOOST_CHECK_EQUAL(hdr.maxTime,TestableMeasCount-1);
-        BOOST_CHECK_EQUAL(hdr.minTime,0);
-        BOOST_CHECK_EQUAL(hdr.size,storage->size());
-    }
-}
 
 BOOST_AUTO_TEST_CASE(StorageCreateOpen){
 	{

174bd57b5bfa7abad1b9d2953280ed1bb0511ea4
Sergey Lyubimov <lysevi@gmail.com>
2015-09-17 21:33:50 +0400

    test: readInterval test
    see #38

---
+++ test/storage_readinterval_test.cpp
@@ -0,0 +1,67 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Main
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+#include "test_common.h"
+#include <storage/page.h>
+#include <storage/storage.h>
+#include <storage/config.h>
+#include <storage/time.h>
+#include "storage/utils/logger.h"
+#include "storage/utils/utils.h"
+
+#include <iterator>
+#include <list>
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+using namespace storage;
+
+BOOST_AUTO_TEST_CASE(StorageReadInterval) {
+    const int meas2write = 5;
+    const uint64_t storage_size = sizeof(storage::Page::Header) + (sizeof(storage::Meas) * meas2write);
+    const std::string storage_path = mdb_test::storage_path + "storageIO";
+    auto ds=storage::Storage::Create(storage_path,storage_size);
+
+    storage::Meas m;
+    {
+        m.id=1; m.time=1;
+        ds->append(m);
+        m.id=2;m.time=2;
+        ds->append(m);
+
+        m.id=4;m.time=4;
+        ds->append(m);
+        m.id=5;m.time=5;
+        ds->append(m);
+        m.id=55;m.time=5;
+        ds->append(m);
+
+        auto reader=ds->readInterval(3,5);
+        storage::Meas::MeasList output{};
+        reader->readAll(&output);
+        BOOST_CHECK_EQUAL(output.size(),size_t(3));
+    }
+    {
+        m.id=1; m.time=6;
+        ds->append(m);
+        m.id=2;m.time=7;
+        ds->append(m);
+
+        m.id=4;m.time=9;
+        ds->append(m);
+        m.id=5;m.time=10;
+        ds->append(m);
+        m.id=55;m.time=10;
+        ds->append(m);
+
+        auto reader=ds->readInterval(8,10);
+        storage::Meas::MeasList output{};
+        reader->readAll(&output);
+        BOOST_CHECK_EQUAL(output.size(),size_t(3));
+    }
+    ds->Close();
+    utils::rm(storage_path);
+}

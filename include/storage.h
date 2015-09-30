9e6e187a8630ce14e6145d43dea2c9bbf8e0f9ba
Sergey Lyubimov <lysevi@gmail.com>
2015-09-30 21:53:40 +0400

    move asyncwriter.

--- include/storage.h
+++ include/storage.h
@@ -6,72 +6,84 @@
 #include <mutex>
 #include "page.h"
 #include "cache.h"
-#include "asyncwriter.h"
+#include "asyncworker.h"
 
 namespace mdb {
 
-	const uint64_t defaultPageSize = sizeof(Page::Header) + sizeof(Meas) * 1000000;
-	const size_t defaultcacheSize = 10000;
-	const size_t defaultcachePoolSize = 100;
+const uint64_t defaultPageSize = sizeof(Page::Header) + sizeof(Meas) * 1000000;
+const size_t defaultcacheSize = 10000;
+const size_t defaultcachePoolSize = 100;
 
 class StorageReader;
 typedef std::shared_ptr<StorageReader> StorageReader_ptr;
+
+class Storage;
+
+class AsyncWriter : public utils::AsyncWorker<Cache::PCache> {
+public:
+  AsyncWriter();
+  void setStorage(Storage *mdb);
+  void call(const Cache::PCache data) override;
+
+private:
+  Storage *m_storage;
+};
 
 /**
 * Main class of mdb storage.
 */
 class Storage {
 public:
-  typedef std::shared_ptr<Storage> Storage_ptr;
+    typedef std::shared_ptr<Storage> Storage_ptr;
 
 public:
-  static Storage_ptr Create(const std::string &ds_path, uint64_t page_size = defaultPageSize);
-  static Storage_ptr Open(const std::string &ds_path);
-  ~Storage();
-  void Close();
+    static Storage_ptr Create(const std::string &ds_path, uint64_t page_size = defaultPageSize);
+    static Storage_ptr Open(const std::string &ds_path);
+    ~Storage();
+    void Close();
 
-  bool havePage2Write() const;
-  append_result append(const Meas& m);
-  append_result append(const Meas::PMeas begin, const size_t meas_count);
+    bool havePage2Write() const;
+    append_result append(const Meas& m);
+    append_result append(const Meas::PMeas begin, const size_t meas_count);
 
-  StorageReader_ptr readInterval(Time from, Time to);
-  StorageReader_ptr readInterval(const IdArray &ids, mdb::Flag source, mdb::Flag flag, Time from, Time to);
-  Meas::MeasList curValues(const IdArray&ids);
+    StorageReader_ptr readInterval(Time from, Time to);
+    StorageReader_ptr readInterval(const IdArray &ids, mdb::Flag source, mdb::Flag flag, Time from, Time to);
+    Meas::MeasList curValues(const IdArray&ids);
 
-  StorageReader_ptr readInTimePoint(Time time_point);
-  StorageReader_ptr readInTimePoint(const IdArray &ids, mdb::Flag source, mdb::Flag flag, Time time_point);
+    StorageReader_ptr readInTimePoint(Time time_point);
+    StorageReader_ptr readInTimePoint(const IdArray &ids, mdb::Flag source, mdb::Flag flag, Time time_point);
 
-  /// get max time in past to write
-  Time pastTime() const;
-  /// set max time in past to write
-  void setPastTime(const Time &t);
+    /// get max time in past to write
+    Time pastTime() const;
+    /// set max time in past to write
+    void setPastTime(const Time &t);
 
-  void enableCacheDynamicSize(bool flg);
-  bool cacheDynamicSize() const;
+    void enableCacheDynamicSize(bool flg);
+    bool cacheDynamicSize() const;
 
-  size_t getPoolSize()const;
-  void setPoolSize(size_t sz);
+    size_t getPoolSize()const;
+    void setPoolSize(size_t sz);
 
-  size_t getCacheSize()const;
-  void setCacheSize(size_t sz);
+    size_t getCacheSize()const;
+    void setCacheSize(size_t sz);
 
-  /// load current values of ids. return array of not founded measurements.
-  IdArray loadCurValues(const IdArray&ids);
+    /// load current values of ids. return array of not founded measurements.
+    IdArray loadCurValues(const IdArray&ids);
 private:
-  Storage();
-  void writeCache();
-  void flush_and_stop();
+    Storage();
+    void writeCache();
+    void flush_and_stop();
 protected:
-  std::string m_path;
+    std::string m_path;
 
-  std::mutex m_write_mutex;
-  mdb::Cache::PCache m_cache;
-  AsyncWriter m_cache_writer;
-  CachePool m_cache_pool;
-  CurValuesCache m_cur_values;
-  Time m_past_time;
-  bool m_closed;
-  friend class mdb::Cache;
+    std::mutex m_write_mutex;
+    mdb::Cache::PCache m_cache;
+    AsyncWriter m_cache_writer;
+    CachePool m_cache_pool;
+    CurValuesCache m_cur_values;
+    Time m_past_time;
+    bool m_closed;
+    friend class mdb::Cache;
 };
 
 class StorageReader: public utils::NonCopy{
@@ -79,7 +91,7 @@ public:
     StorageReader();
     bool isEnd();
     void readNext(Meas::MeasList*output);
-	void readAll(Meas::MeasList*output);
+    void readAll(Meas::MeasList*output);
     void addPage(std::string page_name);
 
     IdArray ids;
@@ -87,10 +99,10 @@ public:
     mdb::Flag flag;
     mdb::Time from;
     mdb::Time to;
-	mdb::Time time_point;
-	std::string prev_interval_page;
+    mdb::Time time_point;
+    std::string prev_interval_page;
 private:
-	std::deque<std::string> m_pages;
+    std::deque<std::string> m_pages;
     PageReader_ptr m_current_reader;
 };
 }
--- libmdb/storage.cpp
+++ libmdb/storage.cpp
@@ -13,6 +13,38 @@ namespace fs = boost::filesystem;
 
 using namespace mdb;
 
+struct MeasCmpByTime {
+  bool operator()(mdb::Meas a, mdb::Meas b) { return a.time < b.time; }
+};
+
+
+AsyncWriter::AsyncWriter() {}
+
+void AsyncWriter::setStorage(Storage *storage) { m_storage = storage; }
+
+void AsyncWriter::call(const Cache::PCache data) {
+  assert(m_storage != nullptr);
+
+  auto output = data->asArray();
+
+  //MeasCmpByTime time_cmp;
+  //std::sort(output, output + data->size(), time_cmp);
+
+  size_t meas_count = data->size();
+  size_t to_write = data->size();
+
+  while (to_write > 0) {
+    size_t writed = PageManager::get()->getCurPage()->append(output + (meas_count - to_write), to_write);
+    if (writed != to_write) {
+        PageManager::get()->createNewPage();
+    }
+    to_write -= writed;
+  }
+  PageManager::get()->getCurPage()->flushWriteWindow();
+  data->clear();
+  data->sync_complete();
+}
+
 
 Storage::Storage()
     : m_cache_pool(defaultcachePoolSize, defaultcacheSize) {

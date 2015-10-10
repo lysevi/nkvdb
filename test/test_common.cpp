ef02cfa92c4c8eef8ada2058144ffca14c125379
Sergey Lyubimov <lysevi@gmail.com>
2015-10-10 19:30:03 +0400

    metatest prototype
    issue #17

--- test/test_common.cpp
+++ test/test_common.cpp
@@ -1,3 +1,9 @@
 #include "test_common.h"
 
-namespace nkvdb_test {}
+namespace nkvdb_test {
+
+    void strage_test_add(nkvdb::MetaStorage*ms,nkvdb::Time from, nkvdb::Time to, nkvdb::Time step){
+
+    }
+
+}
--- test/test_common.h
+++ test/test_common.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <string>
+#include <nkvdb.h>
 
 namespace nkvdb_test {
 const size_t sizeInMb10 = 10 * 1024 * 1024;
 const std::string storage_path = "dstorage";
 const std::string test_page_name = "test_page.db";
+
+void strage_test_add(nkvdb::MetaStorage*ms,nkvdb::Time from, nkvdb::Time to, nkvdb::Time step);
 }

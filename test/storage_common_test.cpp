c1a88ee8d73125d3ac53e03e4b441441c6fbf28c
Sergey Lyubimov <lysevi@gmail.com>
2015-12-25 14:17:04 +0300

    bloom test

--- test/storage_common_test.cpp
+++ test/storage_common_test.cpp
@@ -5,18 +5,18 @@
 #include <bloom_filter.h>
 
 BOOST_AUTO_TEST_CASE(BloomTest) {
-    typedef nkvdb::Bloom<uint8_t> u8_fltr_t;
+    typedef uint8_t u8_fltr_t;
 
-    auto u8_fltr=u8_fltr_t::empty();
+    auto u8_fltr=nkvdb::bloom_empty<u8_fltr_t>();
 
     BOOST_CHECK_EQUAL(u8_fltr, uint8_t{0});
 
-    u8_fltr=u8_fltr_t::add(u8_fltr,uint8_t{1});
-    u8_fltr=u8_fltr_t::add(u8_fltr,uint8_t{2});
+	u8_fltr = nkvdb::bloom_add(u8_fltr, uint8_t{ 1 });
+	u8_fltr = nkvdb::bloom_add(u8_fltr, uint8_t{ 2 });
 
-    BOOST_CHECK(u8_fltr_t::check(u8_fltr,uint8_t{1}));
-    BOOST_CHECK(u8_fltr_t::check(u8_fltr,uint8_t{2}));
-    BOOST_CHECK(u8_fltr_t::check(u8_fltr,uint8_t{3}));
-    BOOST_CHECK(!u8_fltr_t::check(u8_fltr,uint8_t{4}));
-    BOOST_CHECK(!u8_fltr_t::check(u8_fltr,uint8_t{5}));
+	BOOST_CHECK(nkvdb::bloom_check(u8_fltr, uint8_t{ 1 }));
+	BOOST_CHECK(nkvdb::bloom_check(u8_fltr, uint8_t{ 2 }));
+	BOOST_CHECK(nkvdb::bloom_check(u8_fltr, uint8_t{ 3 }));
+	BOOST_CHECK(!nkvdb::bloom_check(u8_fltr, uint8_t{ 4 }));
+	BOOST_CHECK(!nkvdb::bloom_check(u8_fltr, uint8_t{ 5 }));
 }
